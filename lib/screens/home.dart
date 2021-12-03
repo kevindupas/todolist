e9aa41b03564a7297d2d3768782c6f0c6ceb5b6b
Dupas <dupas.dev@gmail.com>
2021-12-03 14:34:10 +0100

    Update home.dart

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -17,26 +17,61 @@ class _HomeState extends State<Home> {
           width: double.infinity,
           padding: const EdgeInsets.symmetric(
             horizontal: 24.0,
-            vertical: 32.0,
           ),
-            color: const Color(0xFFF6F6F6),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
+            color: Color(0xFFF6F6F6),
+            child: Stack(
               children: [
-                Container(
-                  margin: const EdgeInsets.only(
-                    bottom: 32.0,
-                  ),
-                  child: Image.asset(
-                    'assets/images/logo.png',
-                    width: 70,
-                    height: 70,
-                  ),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Container(
+                      margin: const EdgeInsets.only(
+                        top: 32.0,
+                        bottom: 32.0,
+                      ),
+                      child: Image.asset(
+                        'assets/images/logo.png',
+                        width: 70,
+                        height: 70,
+                      ),
+                    ),
+                    Expanded(
+                      child: ListView(
+                        children: const [
+                          TaskCardWidget(
+                            title: "Titre 1",
+                            desc: "Description 1",
+                          ),
+                          TaskCardWidget(),
+                          TaskCardWidget(),
+                          TaskCardWidget(),
+                          TaskCardWidget(),
+                          TaskCardWidget(
+                            title: "Titre 1",
+                            desc: "Description 1",
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
                 ),
-                TaskCardWidget(
-                  title: "Get Started!",
-                ),
-                TaskCardWidget(),
+                Positioned(
+                  bottom: 24.0,
+                  right: 0.0,
+                  child: Container(
+                    width: 60.0,
+                    height: 60.0,
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(20.0),
+                        color: Color(0xFF211551)
+                    ),
+                    child: Image.asset(
+                      'assets/images/add_icon.png',
+                      width: 70,
+                      height: 70,
+                    ),
+                  ),
+                )
               ],
             ),
         ),
