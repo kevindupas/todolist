ce1180bf9f33e1eb9fe60a6091850ec6cb446bea
Dupas <dupas.dev@gmail.com>
2021-12-03 13:34:09 +0100

    Change Image to Image.asset

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -11,20 +11,23 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        padding: EdgeInsets.all(24.0),
-        child: Column(
-          children: const [
-            Image(
-              image: Image.asset(
+      body: SafeArea(
+        child : Container(
+          padding: const EdgeInsets.symmetric(
+            horizontal: 24.0,
+            vertical: 32.0,
+          ),
+            child: Column(
+              children: [
+                Image.asset(
                   'assets/images/logo.png',
-                  height: 100,
-                  width: 100,
-              ),
+                  width: 70,
+                  height: 70,
+                ),
+              ],
             ),
-          ],
         ),
-      ),
+      )
     );
   }
 }
